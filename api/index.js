ea11141cd1927cc7e9d25eb790c485b98238499f
fajarbudi <fajarbudira@gmail.com>
2026-01-29 21:20:00 +0700

    update

--- api/index.js
+++ api/index.js
@@ -94,7 +94,7 @@ app.use(cors());
 const server = http.createServer(app);
 const io = new Server(server, {
   cors: {
-    origin: ["http://localhost:5173", "https://hush-client.vercel.app"],
+    origin: ["http://localhost:5173", "https://hush-client.vercel.app/"],
   },
 });
 
@@ -140,6 +140,6 @@ io.on("connection", (socket) => {
   });
 });
 
-server.listen(80, () => {
+server.listen(3000, () => {
   console.log("🚀 Server running at http://localhost:3001");
 });
